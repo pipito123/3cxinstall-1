ecbb0a911cb7064f1fabb40627f6d68e67f809b7
febo01 <41590154+febo01@users.noreply.github.com>
2019-05-21 11:44:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo "Updating system..."
 sudo apt update --yes
 
 echo "INSTALL new apps"
-sudo apt-get install net-tools tshark wireshark davfs2 cifs-utils net-tools -y
+sudo apt-get install net-tools tshark mtr wireshark davfs2 cifs-utils net-tools -y
 
 cd /home/omadmin/
 wget -o file https://github.com/febo01/3cxinstall/blob/master/Integration.zip
