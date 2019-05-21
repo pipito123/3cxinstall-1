2ec3c25e1720d71b3e79e9a91b1cdabad5f592d3
febo01 <41590154+febo01@users.noreply.github.com>
2019-05-21 10:05:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ echo barbier1510 | command
 
 echo "Install 3CX PBX"
 wget -O- "http://downloads-global.3cx.com/downloads/3cxpbx/public.key" | apt-key add -
-echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list/
+echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 apt-get update
 apt-get install -y 3cxpbx
 
