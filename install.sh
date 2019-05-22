204763b24cda7011f6da43b82d1783daa5a25b75
febo01 <41590154+febo01@users.noreply.github.com>
2019-05-22 11:40:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,15 +11,16 @@ sudo apt-get install net-tools tshark mtr wireshark davfs2 cifs-utils net-tools 
 cd /home/omadmin/
 wget -o file https://github.com/febo01/3cxinstall/blob/master/Integration.zip
 
-
+wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+sudo dpkg -i google-chrome-stable_current_amd64.deb
  
 
 mkdir /mnt/dav
 
 mount -t  davfs  "https://stockage.orizonmobile.com/Technique/3606/Telephonie/3CX/wireshark" /mnt/dav/
 
-echo fbarbier@orizonmobile.com | command
-echo barbier1510 | command
+echo "fbarbier@orizonmobile.com"
+echo "barbier1510"
 
 echo "Install 3CX PBX"
 wget -O- "http://downloads-global.3cx.com/downloads/3cxpbx/public.key" | apt-key add -
@@ -28,3 +29,4 @@ sudo apt-get update
 sudo apt-get install -y 3cxpbx
 
 
+/usr/sbin/3CXWizard --cleanup
