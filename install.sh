09875d0032b3c63619261c46de3e35b4b159d96c
febo01 <41590154+febo01@users.noreply.github.com>
2019-05-23 10:32:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,8 +24,7 @@ echo "fbarbier@orizonmobile.com" && echo "barbier1510"
 
 
 echo "Install 3CX PBX"
-wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add -
+wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key
+apt-key add -
 echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 sudo apt-get update
-&&
-sudo apt-get install 3cxpbx
